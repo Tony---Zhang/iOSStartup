2c3c62aa47dfa1e2ab9a3ceab966c8de5bb6ec0f
Zhang Shuai <520zhangshuai@gmail.com>
2018-03-02 13:08:49 +0800

    Initial Commit

---
+++ Startup/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Startup
+//
+//  Created by Shuai Zhang  on 02/03/2018.
+//  Copyright © 2018 Shuai Zhang . All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
